24132a7169edb1941986f1a6f452ec9db1f94f02
Saranya R <110464294+saranyakpr@users.noreply.github.com>
2024-01-31 00:21:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import AccountOperations from "./Accounts/AccountOperations";
+import BalanceDisplay from "./Accounts/BalanceDisplay";
+
+function App() {
+  return (
+    <div>
+      <BalanceDisplay/>
+      <AccountOperations/>
+    </div>
+  );
+}
+
+export default App;
